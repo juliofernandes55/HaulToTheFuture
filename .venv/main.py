e710b6c5bab2a5ad23c0211a96b52e99cd290353
Julio <j.frnands55@gmail.com>
2025-03-25 22:22:20 -0300

    Initial Commit

---
+++ .venv/main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print("Setup Started")
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+print("Setup Finished")
+
+print("Loop Started")
+while True:
+    #Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #Close Game
+            quit() #End game
