b259f7a1f948c20d7449bfff29fdf9f117f3639d
Julio <j.frnands55@gmail.com>
2025-03-29 14:06:30 -0300

    shots

--- code/Const.py
+++ code/Const.py
@@ -5,10 +5,13 @@ COLOR_PURPLE = (209, 57, 159)
 COLOR_WHITE = (255, 255, 255)
 #E
 EVENT_ENEMY = pygame.USEREVENT + 1
-ENTITY_SPEED = dict(bground0=0, bground1=1, bground2=2, bground3=3, bground4=4, bground5=5, bground6=6, Player=3, Enemy1=2, Enemy2=1, PlayerShot=2, EnemyShot=1,)
-ENTITY_HEALTH = dict(bground0=999, bground1=999, bground2=999, bground3=999, bground4=999, bground5=999, bground6=999, Player=300, PlayerShot=1, Enemy1Shot=1, Enemy2Shot=2,  Enemy1=50, Enemy2=70)
+ENTITY_SPEED = dict(bground0=0, bground1=1, bground2=2, bground3=3, bground4=4, bground5=5, bground6=6, Player=3, Enemy1=2, Enemy2=1, PlayerShot=2, Enemy1Shot=5, Enemy2Shot=2)
+ENTITY_HEALTH = dict(bground0=999, bground1=999, bground2=999, bground3=999, bground4=999, bground5=999, bground6=999, Player=300, PlayerShot=1, Enemy1Shot=1, Enemy2Shot=3,  Enemy1=50, Enemy2=70)
 ENTITY_SHOT_DELAY = {
-    'Player':20
+    'Player':20,
+    'Enemy1':150,
+    'Enemy2':208
+
 }
 #M
 MENU_OPTION = ('PLAY DEMO',
